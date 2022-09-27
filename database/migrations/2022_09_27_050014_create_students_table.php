f353ef79c3938ac4f88da961464e9314f14751b5
Mohamed Sajaath <mohamedsajaath0409@gmail.com>
2022-09-27 12:35:53 +0530

    models controllers migrations database created

---
+++ database/migrations/2022_09_27_050014_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('index_num')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
